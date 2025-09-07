496c6fb89acb6ffe6379fe931870c393bc83d165
Bhenny Hayman <bengero686@gmail.com>
2025-09-07 15:44:58 +0000

    placeholder changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ const SearchBar = () => {
   return (
     <div>
         <div className="flex justify-center items-center  mt-4">
-        <input className="w-[50vw] border-1 border-gray-200 px-2 focus:outline-0 py-1.5" type="text" placeholder=" Enter food here to get recipe"/>
+        <input className="w-[50vw] border-1 border-gray-200 px-2 focus:outline-0 py-1.5" type="text" placeholder=" Enter food here"/>
       <button 
       className="bg-blue-950 text-white h-9 p-1.5 hover:bg-blue-900 cursor-pointer">Search</button>
       </div>
